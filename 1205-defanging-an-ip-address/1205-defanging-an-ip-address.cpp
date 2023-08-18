1b6f9f661ad2e312bf61f91b20c4e8230e1d3a6d
Bhavya Shachi <shachibhavya@gmail.com>
2023-08-19 04:58:29 +0530

    Time: 2 ms (43.61%), Space: 6 MB (20.7%) - LeetHub

---
+++ 1205-defanging-an-ip-address/1205-defanging-an-ip-address.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string defangIPaddr(string address) {
+        string ans;
+        for (char c:address) {
+            if (c == '.') {
+                ans += "[.]";
+            }
+            else ans.push_back(c);
+        }
+        return ans;
+    }
+};
